d2856a6d2e714f09ecae11def1640af196eccbb0
sam2064 <160419308+sam2064@users.noreply.github.com>
2024-06-16 20:31:04 +0530

    Update alu.v

--- alu.v
+++ alu.v
@@ -2,7 +2,7 @@ module alu ( Ra, Rb, alufn, alubeq, alu_out);
     input  [7:0] Ra;
     input  [7:0] Rb;
     input  [2:0] alufn;
-    input  alubeq;
+    output reg  alubeq;
     output reg [7:0] alu_out;
     
 always @(*) begin
@@ -15,17 +15,26 @@ always @(*) begin
             alu_out <= Ra & Rb ;   
         3'b011:
             alu_out <= Ra | Rb;
-      
-        3'b100: ;
-        3'b101: ;
-        3'b110: ;
-        3'b111: 
-           alu_out= alubeq;
-        
+        3'b100: 
+            alu_out <= Ra + Rb;  //addi It will be addition
+        3'b101: 
+            alu_out <= Ra + Rb;  //lw It will be addition (Address Calculation)
+        3'b110: 
+            alu_out <= Ra + Rb;  //sw It will be addition (Address Calculation)
+        3'b111: begin
+           if (Ra==Rb) begin
+            alubeq <= 1;
+           end
+           else begin
+            alubeq <= 0;
+           end
+            
+        end
         default: ;
     endcase
     
 end
     
 endmodule
+
 
