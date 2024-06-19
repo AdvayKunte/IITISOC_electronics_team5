0aa06aec537713628fda2e192f1cd79c34d4724a
sam2064 <160419308+sam2064@users.noreply.github.com>
2024-06-19 14:07:08 +0530

    Add files via upload

---
+++ regfile_mux.v
@@ -0,0 +1,14 @@
+`timescale 1ns / 1ps
+
+module regfile_mux (RB, RDi, RegDst, RDo);
+input [2:0]RB, RDi;
+input RegDst;
+output reg RDo;
+
+case(RegDst)
+        1'b0: RDo <= #2 RB;
+        1'b1: RDo <= #2 RDi;
+        default: ;
+endcase
+    
+endmodule
