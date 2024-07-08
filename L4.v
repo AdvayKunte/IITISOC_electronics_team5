56324a4be0c80fff6ce97d0f9831922bd03ffb4f
sam2064 <160419308+sam2064@users.noreply.github.com>
2024-07-08 12:36:22 +0530

    Update L4.v

--- L4.v
+++ L4.v
@@ -5,21 +5,23 @@ module L4 (alu_in, mem_in, MemToRegmux, alu_out, mem_out, MemToReg, clk2);
 input [7:0] alu_out, mem_out;
 input MemToReg, clk2;
 
-output [7:0] alu_in, mem_in;
-output MemToRegmux;
+output reg [7:0] alu_in, mem_in;
+output reg MemToRegmux;
 
 reg [7:0] L4_alu_out, L4_mem_out;
 reg L4_MemToReg;
 
-assign alu_in = L4_alu_out;
-assign mem_in = L4_mem_out;
-assign MemToRegmux = L4_MemToReg;
-
-    always @(posedge clk2) begin
-    L4_alu_out <= #2 alu_out;
-    L4_mem_out <= #2 mem_out;
+always @(*) begin
+    L4_alu_out <= alu_out;
+    L4_mem_out <= mem_out;
     L4_MemToReg <= MemToReg;
     
+end
+
+always @(posedge clk2) begin
+    alu_in <= L4_alu_out;
+    mem_in <= L4_mem_out;
+    MemToRegmux <= L4_MemToReg;
 end
     
 endmodule
